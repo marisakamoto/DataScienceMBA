8315dada233ad0bfe96ceda3ba6b1ba78f79acac
Mariana Sakamoto <43222644+marisakamoto@users.noreply.github.com>
2021-11-13 19:12:03 -0300

    Intro to R 2 - continue

--- 00_IntroToR.R
+++ 00_IntroToR.R
@@ -1,5 +1,4 @@
 #capital letters matter
-
 object <- 27
 object
 
@@ -37,3 +36,44 @@ numeric_bool
 blood_type <- c('A+','A-','B+','B-','AB+','AB-','O+','O-')
 blood_type <- factor(blood_type)
 blood_type
+
+#categorical variables ordered 
+education_level <- c('primary', 'secondary', 'higher education')
+education_level <- factor(education_level,
+                          levels =  c('primary', 'secondary', 'higher education'))
+education_level #if you dont specify R will order in ASCII
+
+#creating a dataset
+
+company <- c('Company A', 'Company B', NA, 'Company C', 'Company D')
+employers <- c(100, 4000, 200, 15, 35000)
+stock_precence <- c(F, T, NA, T, T)
+national <- c(0,1,1,NA,0)
+ceo_name <- c(NA, 'Joana', 'Carolina','Mary', 'Elizabeth')
+
+##Verify if has the same length
+length(company)
+length(employers)
+length(stock_precence)
+length(national)
+length(stock_precence)
+
+df_companies <- data.frame( company, employers, stock_precence, national, ceo_name)
+df_companies
+View(df_companies) #Use only if the data set is small
+
+## rename the variables
+
+df_companies <- data.frame(company,employees= employers, 
+                           stock_exchange = stock_precence, brazil_hq = national
+                           ,ceo_name)
+
+# Saving objects data from R
+save(df_companies, file = "df_companies.RData")
+# To load it again
+load('df_companies.RData')
+
+load('spam.RData')
+head(spam)
+
+
